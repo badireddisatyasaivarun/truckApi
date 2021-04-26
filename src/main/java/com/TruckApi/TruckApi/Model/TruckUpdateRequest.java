cca7fd24472ff9a3741bce7466ee5929b92b6793
badireddi satya sai varun <badireddi.ssvarun.civ17@itbhu.ac.in>
2021-04-26 17:16:59 +0530

    with test cases ad prev corrections

--- src/main/java/com/TruckApi/TruckApi/Model/TruckUpdateRequest.java
+++ src/main/java/com/TruckApi/TruckApi/Model/TruckUpdateRequest.java
@@ -7,8 +7,5 @@ import lombok.Data;
 public @Data class TruckUpdateRequest {
 	private String imei;
 	private Boolean approved;
-<<<<<<< HEAD
 	private String truckNo;
-=======
->>>>>>> 2b02d77b3e4725b96b71c1f2ea692a7e9f105831
 }
--- src/main/java/com/TruckApi/TruckApi/entities/TruckData.java
+++ src/main/java/com/TruckApi/TruckApi/entities/TruckData.java
@@ -11,17 +11,12 @@ import lombok.Data;
 @Entity
 public @Data class TruckData {
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 2b02d77b3e4725b96b71c1f2ea692a7e9f105831
 	@Id	
 	private String truckId;
 	
 	private String transporterId;
 	private String truckNo;
 	private Boolean approved;
-<<<<<<< HEAD
 	private String imei;
 
 	
@@ -39,10 +34,5 @@ public @Data class TruckData {
 		this.imei = imei;
 	}
 	
-=======
-	private String imei; 
-
-	
->>>>>>> 2b02d77b3e4725b96b71c1f2ea692a7e9f105831
 }
 
